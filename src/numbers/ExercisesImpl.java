712383b941484c415b3f84fd8cdd31dd0f602ef2
Egle Juraityte <egle.juraityte@stud.vtmc.lt>
2022-11-22 21:06:44 +0200

    liko tik filtras

--- src/numbers/ExercisesImpl.java
+++ src/numbers/ExercisesImpl.java
@@ -1,96 +1,104 @@
 package numbers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import lt.itakademija.exam.Exercises;
 import lt.itakademija.exam.IntegerGenerator;
 import lt.itakademija.exam.NumberFilter;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
 public class ExercisesImpl implements Exercises {
-    @Override
-    public Integer findSmallest(List<Integer> list) {
-        return list.stream().min(Integer::compare).get();
-    }
+	@Override
+	public Integer findSmallest(List<Integer> list) {
+		return list.stream().min(Integer::compare).get();
+	}
 
-    @Override
-    public Integer findLargest(List<Integer> list) {
-        return list.stream().max(Integer::compare).get();
-    }
+	@Override
+	public Integer findLargest(List<Integer> list) {
+		return list.stream().max(Integer::compare).get();
+	}
 
-    @Override
-    public boolean isEqual(Object o, Object o1) {
-        return o.equals(o1);//o.hashCode()==o1.hashCode();
-      //  return false;
-    }
+	@Override
+	public boolean isEqual(Object o, Object o1) {
+		return o.equals(o1);// o.hashCode()==o1.hashCode();
+		// return false;
+	}
 
-    @Override
-    public boolean isSameObject(Object o, Object o1) {
+	@Override
+	public boolean isSameObject(Object o, Object o1) {
 
-        return o.hashCode()==o1.hashCode();//o.equals(o1);
-    }
+		return o.hashCode() == o1.hashCode();// o.equals(o1);
+	}
 
-    @Override
-    public List<Integer> consume(Iterator<Integer> iterator) {
-        List<Integer> actualList = new ArrayList<Integer>();
-        iterator.forEachRemaining(actualList::add);
-        return actualList;
-    }
+	@Override
+	public List<Integer> consume(Iterator<Integer> iterator) {
+		List<Integer> actualList = new ArrayList<Integer>();
+		iterator.forEachRemaining(actualList::add);
+		return actualList;
+	}
 
-    @Override
-    public int computeSumOfNumbers(int number) {
-        int sum = 0;
-        for(int i =1; i<=number; i++){
-            sum+=i;
-        }
+	@Override
+	public int computeSumOfNumbers(int number) {
+		int sum = 0;
+		for (int i = 1; i <= number; i++) {
+			sum += i;
+		}
 
-        return sum;
-    }
+		return sum;
 
-    @Override
-    public int computeSumOfNumbers(int number, NumberFilter numberFilter) {
-        int sum = 0;
-        for(int i =1; i<=number; i++){
-            if(numberFilter.accept(i)){
-            sum+=i;
-            }
-        }
+	}
 
-        return sum;
+	@Override
+	public int computeSumOfNumbers(int number, NumberFilter numberFilter) {
+		int sum = 0;
+		for (int i = 1; i <= number; i++) {
+			if (numberFilter.accept(i)) {
+				sum += i;
+			}
+		}
 
-    }
+		return sum;
 
-    @Override
-    public List<Integer> computeNumbersUpTo(int number) {
-        List<Integer> list = new ArrayList<>();
-        for(int i =1; i<number; i++){
-           list.add(i);
-        }
-        return list;
-    }
+	}
 
-    @Override
-    public Map<Integer, Integer> countOccurrences(List<Integer> list) {
-        return null;
-    }
+	@Override
+	public List<Integer> computeNumbersUpTo(int number) {
+		List<Integer> list = new ArrayList<>();
+		for (int i = 1; i < number; i++) {
+			list.add(i);
+		}
+		return list;
+	}
 
-    @Override
-    public IntegerGenerator createIntegerGenerator(int i, int i1) {
-        IntegerGeneratorImpl generator = new IntegerGeneratorImpl();
-        generator.getNext(i,i1);
-        return generator;
-    }
+	@Override
+	public Map<Integer, Integer> countOccurrences(List<Integer> list) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (Integer integer : list) {
 
-    @Override
-    public IntegerGenerator createFilteredIntegerGenerator(IntegerGenerator integerGenerator, NumberFilter numberFilter) {
+			map.put(integer, (map.getOrDefault(integer, 0) + 1));
+		}
+		return map;
 
-//int i = integerGenerator.getNext();
-if(numberFilter.accept(integerGenerator.getNext())){
-    return integerGenerator;
-}
-return null;
-    }
+	}
+
+	@Override
+	public IntegerGenerator createIntegerGenerator(int i, int i1) {
+		IntegerGeneratorImpl generator = new IntegerGeneratorImpl(i, i1);
+		generator.getNext();
+		return generator;
+	}
+
+	@Override
+	public IntegerGenerator createFilteredIntegerGenerator(IntegerGenerator integerGenerator,
+			NumberFilter numberFilter) {
+
+		if (integerGenerator != null) {
+			integerGenerator.getNext();
+			return integerGenerator;
+		}
+		return null;
+	}
 }
--- src/numbers/IntegerGeneratorImpl.java
+++ src/numbers/IntegerGeneratorImpl.java
@@ -1,16 +1,50 @@
 package numbers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import lt.itakademija.exam.IntegerGenerator;
-import java.util.Random;
+import lt.itakademija.exam.NumberFilter;
 
 public class IntegerGeneratorImpl implements IntegerGenerator {
-    @Override
-    public Integer getNext() {
-        //return null;
-        return new Random().nextInt();
-    }
-    public Integer getNext(int min, int max) {
-        Random r = new Random();
-        return r.nextInt((max - min) + 1) + min;
-    }
+
+	private List<Integer> list;
+	private int min;
+	private int max;
+	private int index;
+
+	public IntegerGeneratorImpl(int min, int max) {
+		super();
+		this.min = min;
+		this.max = max;
+		index = 0;
+		list = IntStream.rangeClosed(min - 1, max).boxed().collect(Collectors.toList());
+
+	}
+
+	@Override
+	public Integer getNext() {
+		// return null;
+		Integer value = null;
+		if (index < list.size()) {
+			value = list.get(index);
+			index++;
+		}
+		return value;
+	}
+
+	public Integer filterGetNext(NumberFilter numberFilter) {
+		List<Integer> tempList = new ArrayList();
+		for (Integer integer : list) {
+			if (numberFilter.accept(integer)) {
+				tempList.add(integer);
+			}
+		}
+		list = tempList;
+		return getNext();
+
+	}
+
 }
